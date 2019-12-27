1ebe749822f9ecff71698633ffce402c8ddd4675
m҉4҉x҉X҉.҉!҉ <m4xx101101@gmail.com>
2019-12-27 18:06:49 +0530

    Update host-scanner.py

--- host-scanner.py
+++ host-scanner.py
@@ -4,17 +4,18 @@ import platform
 from datetime import datetime
 import socket    
 
-t1 = datetime.now()
-oper = platform.system()
+t1 = datetime.now() #Taking starting time for time taken calculations
+oper = platform.system() #Checking for OS
 a='.'
-IPAddr = socket.gethostbyname(socket.gethostname())      
-tmp=IPAddr.split('.')
-net1=tmp[0]+a+tmp[1]+a+tmp[2]+a
-#python3 host-scanner.py <specify IP-range with bites =0>
+IPAddr = socket.gethostbyname(socket.gethostname()) #Getting current systems IP      
+tmp=IPAddr.split('.') #Splitting with delimeter '.'
+net1=tmp[0]+a+tmp[1]+a+tmp[2]+a #Making new string accrording to /24 subnet
+#Just a banner
 print ("="*59)
 print("Starting Scannig: {} at ".format(net1)+str(t1))
 print("For {}0/24".format(net1))
 print("="*59)
+#Command according to OS
 if (oper == "Windows"):
    ping1 = "ping -n 1 "
 elif (oper == "Linux"):
@@ -23,20 +24,22 @@ else :
    ping1 = "ping -c 1 "
 
 try:
-    for ip in range(1,255):
+    for ip in range(1,255):  #Brute forcing basically :P 
         fip=net1+str(ip)
         cmd=ping1+fip
         result=os.popen(cmd)
         for line in result.readlines():
             if(line.count("TTL")):
                 print("[+] {} Host is alive on the network".format(fip))
-    t2 = datetime.now()
-    total = t2 - t1
+    t2 = datetime.now() #Getting time after loop is completed
+    total = t2 - t1  #Total time taken 
+    #Just another banner
     print ("="*45)
     print ("Scanning completed in: ",total)
     print ("="*45)
-
+#If keyboard interruption happens 
 except KeyboardInterrupt:
     print("Someone pressed CTRL+C.....")
+#Other errors
 except:
     print("Somthing went wrong!")

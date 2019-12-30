171284ffdcee67227ad9cf8ce493bb63e2177fee
m҉4҉x҉X҉.҉!҉ <m4xx101101@gmail.com>
2019-12-30 17:55:11 +0530

    Update host-scanner.py

--- host-scanner.py
+++ host-scanner.py
@@ -3,8 +3,11 @@ import sys
 import platform
 from datetime import datetime
 import socket    
+import threading
 
-t1 = datetime.now() #Taking starting time for time taken calculations
+time1 = datetime.now() #Taking starting time for time taken calculations
+allips=[]
+threads=[]
 oper = platform.system() #Checking for OS
 a='.'
 IPAddr = socket.gethostbyname(socket.gethostname()) #Getting current systems IP      
@@ -12,8 +15,7 @@ tmp=IPAddr.split('.') #Splitting with delimeter '.'
 net1=tmp[0]+a+tmp[1]+a+tmp[2]+a #Making new string accrording to /24 subnet
 #Just a banner
 print ("="*59)
-print("Starting Scannig: {} at ".format(net1)+str(t1))
-print("For {}0/24".format(net1))
+print("Starting Scannig: {}0/24 at ".format(net1)+str(time1))
 print("="*59)
 #Command according to OS
 if (oper == "Windows"):
@@ -23,23 +25,28 @@ elif (oper == "Linux"):
 else :
    ping1 = "ping -c 1 "
 
-try:
-    for ip in range(1,255):  #Brute forcing basically :P 
+for ip in range(1,255):  #Brute forcing basically :P 
         fip=net1+str(ip)
         cmd=ping1+fip
-        result=os.popen(cmd)
-        for line in result.readlines():
-            if(line.count("TTL")):
-                print("[+] {} Host is alive on the network".format(fip))
-    t2 = datetime.now() #Getting time after loop is completed
-    total = t2 - t1  #Total time taken 
-    #Just another banner
-    print ("="*45)
-    print ("Scanning completed in: ",total)
-    print ("="*45)
-#If keyboard interruption happens 
-except KeyboardInterrupt:
-    print("Someone pressed CTRL+C.....")
-#Other errors
-except:
-    print("Somthing went wrong!")
+        allips.append(cmd)
+
+def scan(a): 
+    for line in os.popen(a).readlines():
+        if(line.count("TTL")):
+            print("[+] {} Host is alive on the network".format(line))
+
+
+
+for xyz in allips:
+    t1 = threading.Thread(target=scan,args=(xyz,))
+    t1.start()
+
+t1.join()
+
+
+time2 = datetime.now() #Getting time after loop is completed
+total = time2 - time1  #Total time taken 
+#Just another banner
+print ("="*45)
+print ("Scanning completed in: ",total)
+print ("="*45)
